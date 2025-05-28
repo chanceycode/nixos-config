cf4bdbc07c779b0f9075498bd87f14f3aef87662
chanceycode <139152739+chanceycode@users.noreply.github.com>
2025-05-28 19:22:47 -0400

    feat: add hyprland cachix to substituters

--- flake.nix
+++ flake.nix
@@ -128,6 +128,12 @@
                 LC_TIME = "en_US.UTF-8";
               };
 
+              nix.settings = {
+                substituters = ["https://hyprland.cachix.org"];
+                trusted-substituters = ["https://hyprland.cachix.org"];
+                trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+              };
+
               # Enable the X11 windowing system.
               services.xserver.enable = true;
 
