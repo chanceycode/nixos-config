7e6d806db0be8b69fe31449f051070c8d300191c
John D. Chancey <139152739+chanceycode@users.noreply.github.com>
2025-06-01 11:56:13 -0400

    fix: correct host name (#35)
    * chore: remove redundant todo comment
    
    * fix: replace with correct hostname

--- flake.nix
+++ flake.nix
@@ -35,7 +35,6 @@
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
-      # FIXME replace with your hostname
       starcraft = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules = [
@@ -101,7 +100,7 @@
               boot.loader.efi.canTouchEfiVariables = true;
 
               boot.initrd.luks.devices."luks-e99ba237-093b-4b05-96f6-e0fb4d270859".device = "/dev/disk/by-uuid/e99ba237-093b-4b05-96f6-e0fb4d270859";
-              networking.hostName = "nixos"; # Define your hostname.
+              networking.hostName = "starcraft"; # Define your hostname.
               # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
               # Configure network proxy if necessary
